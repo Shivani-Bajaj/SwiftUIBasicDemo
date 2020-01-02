a54bb7c9f6456b5d124d887477531db771825fbd
Shivani-Bajaj <shivani.bajaj9@gmail.com>
2020-01-02 15:23:23 +0530

    Initial Commit

---
+++ LandmarksSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandmarksSwiftUIDemo
+//
+//  Created by shivani Bajaj on 1/2/20.
+//  Copyright © 2020 Shivani Bajaj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
